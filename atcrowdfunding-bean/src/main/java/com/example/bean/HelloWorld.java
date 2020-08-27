c4ef338a85b01194e2824624a49402174c4b3c39
lace <ronaldqinbiao@gmail.com>
2020-08-27 17:20:14 +0800

    打包运行调试。

---
+++ atcrowdfunding-bean/src/main/java/com/example/bean/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.example.bean;
+
+public class HelloWorld {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void hello() {
+		System.out.println("Hello, " + name + "!");
+	}
+}
